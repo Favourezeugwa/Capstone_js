921ae444ca686c0d107933591f12cd1679a2017a
Shahnawaz Ali <shahnawaza75@gmail.com>
2022-06-22 15:59:29 +0530

    Add list items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+import displayMatches from './modules/render.js';
+
+const matchesContainer = document.getElementById('matchesContainer');
+const baseurl = 'https://www.scorebat.com/video-api/v3/feed/?token=MjA5OThfMTY1NTg5MTUwOF80OTRiYzkwMjE0MDFiYzdmZmY2ZGRhOTY3NmVkOWRjZTFjM2Q4Y2M0';
+displayMatches(matchesContainer, baseurl);
